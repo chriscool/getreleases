9bb98facb1a8cfa30a2df948832160ecacdefb5b
Christian Couder <christian.couder@gmail.com>
2022-12-08 17:33:31 +0100

    publish_edition: small cleanups and comments

--- publish_edition.sh
+++ publish_edition.sh
@@ -19,71 +19,28 @@
 # wednesday.
 #
 
-arg="$1"
 
-die() {
-    printf >&2 "FATAL: %s\n" "$@"
-    exit 1
-}
-
-test "$#" -le 1 ||
-	die "too many arguments" "Usage: $0 [<next date>]"
-
-today=$(date "+%F")
-
-if test -n "$arg"
-then
-	nextdate=$(date "+%F" --date="$arg") ||
-		die "failed to understand '$arg' as a date"
-else
-	nextdate=$(date "+%F" --date="$today + 1 month")
-	day_of_week=$(date "+%u" --date="$nextdate")
-	test "$day_of_week" -lt 3 &&
-		nextdate=$(date "+%F" --date="$nextdate + $(( 3 - $day_of_week )) days")
-	test "$day_of_week" -gt 3 &&
-		nextdate=$(date "+%F" --date="$nextdate + $(( 10 - $day_of_week )) days")
-fi
-
-echo "Next publication date is: $(LANG=C date "+%A %B %d, %Y" --date="$nextdate")"
-
-basedir=$(dirname "$0")
+# Repo related constants
 
 repo_url="https://github.com/git/git.github.io.git"
 known_good_commit="5bc243932ea7938830757e8370df6bd86df39cab"
 src_dir="rev_news/drafts"
 dst_dir="_posts"
-
-# Basic checks
-
-type git >/dev/null || die "git not found" "we need git"
-
-git show "$known_good_commit" >/dev/null 2>&1 ||
-	die "$known_good_commit not found" \
-	    "we need to be in a repo cloned from $repo_url"
-
-cur_branch=$(git rev-parse --abbrev-ref HEAD)
-test "$cur_branch" = "master" || die "please switch to the 'master' branch"
-
-test -d "$src_dir" || die "no source '$src_dir' directory"
-
-test -d "$dst_dir" || die "no destination '$dst_dir' directory"
-
-nb_ed=$(ls "$src_dir"/edition-*.md | wc -l)
-
-test "$nb_ed" -eq 0 && die "no 'edition-*.md' file in '$src_dir' directory"
-
-test "$nb_ed" -gt 1 && die "more than one 'edition-*.md' file in '$src_dir' directory"
+main_branch="master"
 
 
-# Find info we need
+# Useful infos from environments
 
-edition=$(ls "$src_dir"/edition-*.md)
+today=$(date "+%F")
+basedir=$(dirname "$0")
 
-cur=$(expr "$edition" : "rev_news/drafts/edition-\([0-9]\+\).md")
 
-test -n "$cur" || die "'$edition' should contain a number"
+# Helper functions
 
-next=$(expr "$cur" + 1)
+die() {
+    printf >&2 "FATAL: %s\n" "$@"
+    exit 1
+}
 
 add_order_suffix() {
 perl -e '
@@ -100,7 +57,66 @@ perl -e '
 ' "$1"
 }
 
+
+# Process arguments
+
+arg="$1"
+
+test "$#" -le 1 ||
+	die "too many arguments" "Usage: $0 [<next date>]"
+
+
+# Compute nextdate, the date for the next edition
+
+if test -n "$arg"
+then
+	nextdate=$(date "+%F" --date="$arg") ||
+		die "failed to understand '$arg' as a date"
+else
+	nextdate=$(date "+%F" --date="$today + 1 month")
+	day_of_week=$(date "+%u" --date="$nextdate")
+	test "$day_of_week" -lt 3 &&
+		nextdate=$(date "+%F" --date="$nextdate + $(( 3 - $day_of_week )) days")
+	test "$day_of_week" -gt 3 &&
+		nextdate=$(date "+%F" --date="$nextdate + $(( 10 - $day_of_week )) days")
+fi
+
+echo "Next publication date is: $(LANG=C date "+%A %B %d, %Y" --date="$nextdate")"
+
+
+# Basic checks
+
+type git >/dev/null || die "git not found" "we need git"
+
+git show "$known_good_commit" >/dev/null 2>&1 ||
+	die "$known_good_commit not found" \
+	    "we need to be in a repo cloned from $repo_url"
+
+cur_branch=$(git rev-parse --abbrev-ref HEAD)
+test "$cur_branch" = "$main_branch" || die "please switch to the '$main_branch' branch"
+
+test -d "$src_dir" || die "no source '$src_dir' directory"
+test -d "$dst_dir" || die "no destination '$dst_dir' directory"
+
+nb_ed=$(ls "$src_dir"/edition-*.md | wc -l)
+
+test "$nb_ed" -eq 0 && die "no 'edition-*.md' file in '$src_dir' directory"
+test "$nb_ed" -gt 1 && die "more than one 'edition-*.md' file in '$src_dir' directory"
+
+
+# Find info we need
+
+edition=$(ls "$src_dir"/edition-*.md)
+
+cur=$(expr "$edition" : "rev_news/drafts/edition-\([0-9]\+\).md")
+
+test -n "$cur" || die "'$edition' should contain a number"
+
+next=$(expr "$cur" + 1)
+
+
 # Each edition covers the previous month
+
 next_month=$(LANG=C date "+%B %Y")
 
 f_day=$(LANG=C date "+%-d" --date="$nextdate")
@@ -108,11 +124,13 @@ f_month=$(LANG=C date "+%B" --date="$nextdate")
 f_year=$(LANG=C date "+%Y" --date="$nextdate")
 full_date="$f_month $(add_order_suffix $f_day), $f_year"
 
+
 # Publish current draft
 
 git mv "$src_dir"/edition-$cur.md "$dst_dir"/$today-edition-$cur.markdown
 
 git commit -m "Publish rn-$cur in $dst_dir/"
+
 
 # Create a draft for next edition
 
